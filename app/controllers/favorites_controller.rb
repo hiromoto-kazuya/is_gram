c0ae8098acda0432d3cc7c985dae31bbe1011086
廣本 和也 <kazuya@kazuya-macbook.local>
2018-08-19 10:49:30 +0900

    flash微修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,11 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.create(post_id: params[:post_id])
-    redirect_to posts_url, notice: "#{favorite.post.user_id}の投稿をお気に入りしました"
+    redirect_to posts_url, notice: "#{favorite.post.user.name}の投稿をお気に入りしました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to posts_url, notice: "#{favorite.post.user_id}の投稿をお気に入り解除しました"
+    redirect_to posts_url, notice: "#{favorite.post.user.name}の投稿をお気に入り解除しました"
   end
 end
