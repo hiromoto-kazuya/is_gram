d2069bc5654533bbff0b7f82f5d7f2c32aea7f6b
廣本 和也 <kazuya@kazuya-macbook.local>
2018-08-25 13:44:17 +0900

    userコントローラーfavoriteアクション内変数@post削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
 
   def favorites
     @user = User.find(params[:id])
-    @post = Post.find(params[:id])
+    # @post = Post.find(params[:id])
     if @user.favorites
       @favorites = Favorite.where(user_id: session[:user_id]).order(created_at: :desc)
     end
