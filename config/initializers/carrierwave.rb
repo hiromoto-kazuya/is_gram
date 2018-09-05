65c35ee7e6aa2a0aabe0d1159e8cbd907aa87a39
廣本 和也 <kazuya@kazuya-macbook.local>
2018-09-05 18:15:58 +0900

    change storage to fog from file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,13 +12,10 @@ CarrierWave.configure do |config|
 
   case Rails.env
     when 'production'
-      config.fog_directory = '本番環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+      config.fog_directory = 'isgram-clone-production'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/isgram-clone-production'
     when 'development'
-      config.fog_directory = '開発環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
-    when 'test'
-      config.fog_directory = 'テスト環境用のバケット名'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/テスト環境用のバケット名'
+      config.fog_directory = 'isgram-clone-development'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/isgram-clone-development'
   end
 end
