dc2c6196e7c413220da57b75feff2255900813de
廣本 和也 <kazuya@kazuya-macbook.local>
2018-09-06 18:04:10 +0900

    お気に入りAjax追加、headerデザイン改善

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,11 +1,15 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.create(post_id: params[:post_id])
-    redirect_to posts_url, notice: "#{favorite.post.user.name}の投稿をお気に入りしました"
+    respond_to do |format|
+      format.js { render :template => "posts/favorite",locals: { favorite: favorite, post: favorite.post } }
+    end
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to posts_url, notice: "#{favorite.post.user.name}の投稿をお気に入り解除しました"
+    respond_to do |format|
+      format.js { render :template => "posts/favorite", locals: { favorite: favorite, post: favorite.post } }
+    end
   end
 end
