0432c3388eb0257fcd8cd7778634bda779764544
廣本 和也 <kazuya@kazuya-macbook.local>
2018-09-20 16:51:57 +0900

    production.rb デプロイ先ホストURL修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   #下記mailer実装のため追記
-  config.action_mailer.default_url_options = { host: '自分のHerokuアプリのドメイン' }
+  config.action_mailer.default_url_options = { host: 'https://clonegram-kazuya.herokuapp.com/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
